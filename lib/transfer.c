2fb5a645b245a804a9965d7137f94acf62b5ac1a
Stefan Eissing <stefan@eissing.org>
2025-05-26 13:21:59 +0200

    Curl_conn_recv(), adjust pnread type
    Parameter `pnread` was a `ssizet_t *`, but `size_t *` is better
    since the function returns any error in its `CURLcode` return value.
    
    Change and adjust usages in other code.

--- lib/transfer.c
+++ lib/transfer.c
@@ -210,7 +210,7 @@ static ssize_t xfer_recv_resp(struct Curl_easy *data,
                               bool eos_reliable,
                               CURLcode *err)
 {
-  ssize_t nread;
+  size_t nread;
 
   DEBUGASSERT(blen > 0);
   /* If we are reading BODY data and the connection does NOT handle EOF
@@ -251,8 +251,7 @@ static ssize_t xfer_recv_resp(struct Curl_easy *data,
     }
     DEBUGF(infof(data, "sendrecv_dl: we are done"));
   }
-  DEBUGASSERT(nread >= 0);
-  return nread;
+  return (ssize_t)nread;
 }
 
 /*
@@ -948,7 +947,7 @@ CURLcode Curl_xfer_send(struct Curl_easy *data,
 
 CURLcode Curl_xfer_recv(struct Curl_easy *data,
                         char *buf, size_t blen,
-                        ssize_t *pnrcvd)
+                        size_t *pnrcvd)
 {
   int sockindex;
 
